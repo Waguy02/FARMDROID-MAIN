f17a78462abc6a56aa2d9b5438818868084be54e
Rudy-ENSP <rudyfosso@gmail.com>
2020-01-06 16:08:23 +0100

    Mofification des Traitement

--- traitements/decisionnal_core/Mesures.js
+++ traitements/decisionnal_core/Mesures.js
@@ -1,13 +1,30 @@
 
+/*var MesureSet= {
+    var eau=0.3
+    var temperature_sol
+    var temperature_air
+    var humidite_air
+    var cO2
+    var luminosite
+    var ph_sol
+
+}*/
+    var eau=0.3
+    var temperature_sol
+    var temperature_air
+    var humidite_air
+    var cO2
+    var luminosite
+    var ph_sol
 
 
-module.exports=class MesureSet{
-temperature_sol;
-temperature_air;
-humidite_air;
-cO2;
-luminosite;
-ph_sol;
-eau;
+module.exports={
+temperature_sol,
+temperature_air,
+humidite_air,
+cO2,
+luminosite,
+ph_sol,
+eau,
 }
 
