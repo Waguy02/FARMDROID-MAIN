ddf234a4a4378de00ed06e3f0d09c515b4a9be78
Rudy-ENSP <rudyfosso@gmail.com>
2020-01-07 07:44:25 +0100

    Mofification des Traitement

--- traitements/rules/mais_normal/water_estimation.pl
+++ traitements/rules/mais_normal/water_estimation.pl
@@ -8,6 +8,6 @@ co2(0,000000.2).
 luminosite(400,600).
 temperature_sol(20,40).
 
-dimunition_eau(0.3):- temperature_sol(X,Y),X>=40,Y>X.
-dimunition_eau(0.1):- luminosite(X,Y),X>=600,Y>X.
-dimunition_eau(0.2):-temperature_air(X,Y),X>=40,Y>X.
+dimunition_eau(0.2):- temperature_sol(X,Y),X>=40,Y>X.
+dimunition_eau(0.01):- luminosite(X,Y),X>=600,Y>X.
+dimunition_eau(0.1):-temperature_air(X,Y),X>=40,Y>X.
